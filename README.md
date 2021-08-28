264bc494659941c1ff64e59cb3e8732c541587ab
randhirkr <cusatrandhir@gmail.com>
2021-08-27 21:25:14 -0700

    added logic for page content

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # stackoverflow-crawler
 stackoverflow crawler for AWS related questions.
+
+
+
+### Reference
+https://blog.pusher.com/introduction-web-scraping-python/
--- crawler.py
+++ crawler.py
@@ -1,7 +1,22 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import requests
 
-with urlopen('https://stackoverflow.com/questions') as response:
-    soup = BeautifulSoup(response, 'html.parser')
-    for anchor in soup.find_all('a'):
-        print(anchor.get('href', '/'))
+# https://stackoverflow.com/questions/tagged/amazon-web-services?tab=Unanswered
+# https://stackoverflow.com/questions/tagged/amazon-web-services?tab=Votes
+# https://stackoverflow.com/questions/tagged/amazon-web-services?tab=Bounties
+
+resp = requests.get('https://stackoverflow.com/questions/tagged/amazon-web-services?tab=Newest')
+if resp.status_code == 200:
+    print(resp.content)
+
+# Fetches all anchor links in page
+def fetchlinks():
+    with urlopen('https://stackoverflow.com/questions/tagged/amazon-web-services?tab=Newest') as response:
+        soup = BeautifulSoup(response, 'html.parser')
+        for anchor in soup.find_all('a'):
+            print(anchor.get('href', '/'))
+
+
+if __name__ == '__main__':
+    print("fetch links")
