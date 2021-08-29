661b4cca1cb3f146c70f0e25403e123855aa461c
randhirkr <cusatrandhir@gmail.com>
2021-08-28 21:12:08 -0700

    modified crawler file

--- crawler.py
+++ crawler.py
@@ -5,8 +5,9 @@ import requests
 # https://stackoverflow.com/questions/tagged/amazon-web-services?tab=Unanswered
 # https://stackoverflow.com/questions/tagged/amazon-web-services?tab=Votes
 # https://stackoverflow.com/questions/tagged/amazon-web-services?tab=Bounties
-
-response = requests.get('https://stackoverflow.com/questions/tagged/amazon-web-services?tab=Newest')
+# fetch latest question with AWS tag on stackoverflow
+url = 'https://stackoverflow.com/questions/tagged/amazon-web-services?tab=Newest'
+response = requests.get(url)
 if response.status_code == 200:
     page_content = BeautifulSoup(response.content, 'html.parser')
     question_list = page_content.find_all(class_='question-summary')
@@ -17,15 +18,3 @@ if response.status_code == 200:
         url = question.find(class_='question-hyperlink').get('href', '/')
         print("https://stackoverflow.com"+url)
         print()
-
-
-# Fetches all anchor links in page
-def fetchlinks():
-    with urlopen('https://stackoverflow.com/questions/tagged/amazon-web-services?tab=Newest') as response:
-        soup = BeautifulSoup(response, 'html.parser')
-        for anchor in soup.find_all('a'):
-            print(anchor.get('href', '/'))
-
-
-if __name__ == '__main__':
-    print("fetch links")
